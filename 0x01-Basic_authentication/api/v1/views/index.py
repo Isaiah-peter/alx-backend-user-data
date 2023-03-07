ea9354edcc0b0e9344336c780af8cfbad6d3442c
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-07 17:56:18 +0100

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route("/unauthorized", strict_slashes=False)
+def unauthorized() -> str:
+    """GET /app/v1/unauthorized
+    Return:
+      - unauthorized
+    """
+    return abort(401)
