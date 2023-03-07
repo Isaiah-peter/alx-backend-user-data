1b780dbe1cd7fded54337553131b0f03c4ece06f
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-07 18:18:54 +0100

    auth setup

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""authentication"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """requires auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
