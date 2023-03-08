2ee76ddc15743fa786a9f39ffda72041f020dd3d
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-08 17:02:45 +0100

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,11 +11,11 @@ class BasicAuth(Auth):
         """extract base64 authorization header"""
         if authorization_header is None:
             return None
-        
+
         if type(authorization_header) != str:
             return None
-        
+
         if authorization_header[0:6] != "Basic ":
             return None
-        
+
         return authorization_header[6:]
