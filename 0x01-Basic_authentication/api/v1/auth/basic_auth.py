aafdc66539b48185607cbeaf9a40d7cb4b01f720
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-08 17:50:47 +0100

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Basic Auth"""
 
+import binascii
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -20,3 +22,19 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header"""
+        if base64_authorization_header == None:
+            return None
+
+        if type(base64_authorization_header) != str:
+            return None
+
+        try:
+            data_decode = b64decode(base64_authorization_header)
+        except binascii.Error as err:
+            return None
+
+        return data_decode.decode('utf-8')
