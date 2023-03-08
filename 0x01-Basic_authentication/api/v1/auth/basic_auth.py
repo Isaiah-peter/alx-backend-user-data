cb63fd5b4672ec96cceb13f692f0322e8ec9538c
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-08 18:23:32 +0100

    task 10 and 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 import binascii
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import TypeVar, List
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -54,3 +56,55 @@ class BasicAuth(Auth):
         email, password = decoded_base64_authorization_header.split(':')
 
         return (email, password)
+
+
+def user_object_from_credentials(
+        self, user_email: str, user_pwd: str) -> TypeVar('User'):
+    """user_object_from_credentials"""
+
+    if user_email is None and type(user_email) != str:
+        return None
+
+    if user_pwd is None and type(user_pwd) != str:
+        return None
+
+    try:
+        exist_user: List[TypeVar('User')]
+        exist_user = User.search({"email": user_email})
+    except Exception:
+        return None
+
+    for user in exist_user:
+        if user.is_valid_password(user_pwd):
+            return user
+
+    return None
+
+
+def current_user(self, request=None) -> TypeVar('User'):
+    """current user"""
+    header: str = self.authorization_header(request)
+
+    if header is None:
+        return None
+
+    auth_head64: str = self.extract_base64_authorization_header(header)
+
+    if auth_head64 is None:
+        return None
+
+    decode_auth: str = self.decode_base64_authorization_header(auth_head64)
+
+    if decode_auth is None:
+        return decode_auth
+
+    mail: str
+    passwd: str
+    mail, passwd = self.extract_user_credentials(decode_auth)
+
+    if mail is None or passwd is None:
+        return None
+
+    user_curr = self.user_object_from_credentials(mail, passwd)
+
+    return user_curr
