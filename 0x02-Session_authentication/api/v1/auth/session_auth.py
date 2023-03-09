802298ffa55dd7dc00f6df7c714d26f3331b181c
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-09 18:35:10 +0100

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,3 +22,13 @@ class SessionAuth(Auth):
 
         self.user_id_by_session_id[id] = user_id
         return id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """user id for session"""
+        if session_id is None:
+            return None
+
+        if type(session_id) != str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
