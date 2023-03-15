71435da564cf377ada223fb3ffc730d842843f24
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-15 16:54:57 +0100

    please work

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 """create user model"""
+from sqlalchemy import String, Integer, Column
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
+Base = declarative_base()
 
-
-class Base(DeclarativeBase):
-    pass
 
 class User(Base):
     """user table"""
-
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(250), nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(250), nullable=False)
-    session_id: Mapped[str] = mapped_column(String(250), nullable=True)
-    reset_token: Mapped[str] = mapped_column(String(250), nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
