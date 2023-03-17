02696771ae2dd4e9b1133e88132dcad6077ea8da
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-17 19:57:17 +0100

    task2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,3 +38,19 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, *arg, **kwargs) -> User:
+        """find by user id, email .."""
+
+        if not kwargs:
+            raise InvalidRequestError()
+
+        col_keys = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in col_keys:
+                raise InvalidRequestError()
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound()
+        return user
